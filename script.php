39cd0db49ac0cc9735ccbc8d43c087574da55bc1
Joomill <info@joomill.nl>
2020-06-01 12:49:48 +0200

    V3.6.0 - Added Version in Copyright

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  package: Joomla Opening Hours module
+ *  package: Joomla Opening Hours module - FREE Version
  *  copyright: Copyright (c) 2020. Jeroen Moolenschot | Joomill
  *  license: GNU General Public License version 3 or later
  *  link: https://www.joomill-extensions.com
